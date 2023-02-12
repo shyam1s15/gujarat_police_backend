21e92c4573da0133f1d5b0bb14b81d3e2de91195
shyam kachhadiya <shyam1ss15@gmail.com>
2023-02-12 20:11:14 +0530

    akjnfkanf

--- src/main/java/com/shyam/gujarat_police/GujaratPoliceApplication.java
+++ src/main/java/com/shyam/gujarat_police/GujaratPoliceApplication.java
@@ -3,10 +3,6 @@ package com.shyam.gujarat_police;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-class Student extends Exception { }
-
-class Hostelller extends Student {}
-
 @SpringBootApplication
 public class GujaratPoliceApplication {
 	public static void main(String[] args) {
